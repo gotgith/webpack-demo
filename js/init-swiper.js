9a67fb020cd69a67898788f291736a64bce6ae2d
JH <18468107890@163.com>
2019-06-11 00:08:47 +0800

    MVC模式管理代码

---
+++ js/init-swiper.js
@@ -0,0 +1,31 @@
+!function () {
+    var view = document.querySelector('#mySlides')
+    var controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: {
+            // 是否无缝
+            loop: true,
+            // 是否需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            // 是否需要上下按钮
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
+        },
+        init: function (view) {
+            this.view = view
+            this.initSwiper()
+        },
+        initSwiper: function () {
+            this.swiper = new Swiper(
+                this.view.querySelector('.swiper-container'),
+                this.swiperOptions
+            )
+        }
+    }
+    controller.init(view)
+}.call()
